40ed21ef79c9c4c6d321fe15de935cf131fddc5a
Zalaya <enol.corzal@gmail.com>
2025-06-07 21:00:46 +0200

    enhancement: remove cqrs to see if we really need it

--- src/main/java/dev/zalaya/kora/domain/command/Command.java
+++
@@ -1,5 +0,0 @@
-package dev.zalaya.kora.domain.command;
-
-public interface Command<R> {
-
-}
--- src/main/java/dev/zalaya/kora/domain/command/CommandHandler.java
+++
@@ -1,7 +0,0 @@
-package dev.zalaya.kora.domain.command;
-
-public interface CommandHandler<R, C extends Command<R>> {
-
-    R handle(C command);
-
-}
